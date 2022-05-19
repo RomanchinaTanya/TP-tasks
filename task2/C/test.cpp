4cd7a207c495052f24a994bf10efc59a37be6c14
RomanchinaTanya <romanchina.ts@phystech.edu>
2022-05-19 23:56:35 +0300

    task2 fixed #3

--- task2/C/test.cpp
+++ task2/C/test.cpp
@@ -4,22 +4,12 @@
 #include "B/lib.h"
 
 TEST(CHECK, GET_HELLO_WORLD_MESSAGE) {
-ASSERT_EQ(GetHelloWorldMessage(), "Hello, world!\n");
+    ASSERT_EQ(GetHelloWorldMessage(), "Hello, world!\n");
 }
 
-TEST(CHECK, SUM_POSITIVE) {
-ASSERT_EQ(Sum(12345, 54321), 66666);
+TEST(CHECK, SUM) {
+    ASSERT_EQ(Sum(12345, 54321), 66666);
+    ASSERT_EQ(Sum(-1000, -100), -1100);
+    ASSERT_EQ(Sum(22, 0), 22);
+    ASSERT_EQ(Sum(-345665, 345665), 0);
 }
-
-TEST(CHECK, SUM_NEGATIVE) {
-ASSERT_EQ(Sum(-1000, -100), -1100);
-}
-
-TEST(CHECK, SUM_ZERO) {
-ASSERT_EQ(Sum(22, 0), 22);
-}
-
-TEST(CHECK, SUM_OPPOSITE) {
-ASSERT_EQ(Sum(-345665, 345665), 0);
-}
-
